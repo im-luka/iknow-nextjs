8e2978108254de5ac1ac00232acbac8618955b5e
Luka Dusak <lux.9833@gmail.com>
2022-08-25 22:12:45 +0200

    please be working

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -50,9 +50,6 @@ export default NextAuth({
         "https://accounts.spotify.com/authorize?scope=user-modify-playback-state,user-follow-modify,user-read-recently-played,user-read-playback-position,playlist-read-collaborative,user-read-playback-state,user-read-email,streaming,user-top-read,playlist-modify-public,user-library-modify,user-follow-read,user-read-currently-playing,user-library-read,playlist-read-private,user-read-private,playlist-modify-private",
     }),
   ],
-  pages: {
-    signIn: "/auth/login",
-  },
   callbacks: {
     async jwt({ token, user, account }) {
       // Initial sign in
