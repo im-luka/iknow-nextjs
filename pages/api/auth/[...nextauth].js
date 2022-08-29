95f878fa9901fc4a3989e2f4f38aa8d7bdaa5019
Luka <lux.9833@gmail.com>
2022-08-29 19:06:52 +0200

    fixed env naming

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -29,8 +29,8 @@ const refreshAccessToken = async (token) => {
 export default NextAuth({
   providers: [
     SpotifyProvider({
-      clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
       authorization: LOGIN_URL,
     }),
   ],
