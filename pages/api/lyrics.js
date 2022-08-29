799ad227e684a50ef9b505d3f2ed99be1ade142d
Luka <lux.9833@gmail.com>
2022-08-29 17:53:56 +0200

    fix 2

--- pages/api/lyrics.js
+++ pages/api/lyrics.js
@@ -1,6 +1,15 @@
 import LyricsFinder from "lyrics-finder";
 
 async function handler(req, res) {
+  res.json({
+    a: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
+    b: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
+    c: process.env.NEXTAUTH_URL,
+    d: process.env.NEXTAUTH_SECRET,
+  });
+
+  return;
+
   if (req.method !== "POST") res.json({ message: "not post method" });
 
   const { name, artist } = req.body;
